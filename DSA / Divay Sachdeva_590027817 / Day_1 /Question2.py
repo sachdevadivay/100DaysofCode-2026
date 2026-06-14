fa9ccf1a5cfb10fed68a83d079930e6947e171ba
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-15 00:07:30 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 / Day_1 /Question2.py
+++ DSA / Divay Sachdeva_590027817 / Day_1 /Question2.py
@@ -1,1 +1,10 @@
+# Write an efficient function to calculate the sum of all elements in an integer array.
 
+def arraySum(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+arr = list(map(int, input().split()))
+print(arraySum(arr))
