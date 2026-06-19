f71e7d33a2444027a9fac8718a945afb6d89e224
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-19 23:13:47 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_5/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_5/Question2.py
@@ -1,1 +1,9 @@
+# Given two arrays, determine whether they are equal. Two arrays are considered equal if they have the same size and corresponding elements are identical..
 
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+if sorted(arr1) == sorted(arr2):
+    print(True)
+else:
+    print(False)
